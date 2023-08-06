2fa7045a732c2beaf187a89145e6f79ffb8787b4
joaovwfreire <jvwfreire@gmail.com>
2023-08-05 23:24:20 -0300

    getenv instead of dotenv

--- database/connect.go
+++ database/connect.go
@@ -6,17 +6,9 @@ import (
 	"os"
 
 	_ "github.com/go-sql-driver/mysql"
-
-	"github.com/joho/godotenv"
 )
 
 func connectDB() (*sql.DB, error) {
-
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatalf("err loading: %v", err)
-		return nil, err
-	}
 
 	db, err := sql.Open("mysql", os.Getenv("DSN"))
 	if err != nil {
